3788965546eb36f4ca69b736c0cb0699b8ca4bdf
Gareth Fultz <garethfultz@gmail.com>
2024-11-06 16:56:55 -0500

    Added cors

--- src/backend/run.py
+++ src/backend/run.py
@@ -1,8 +1,8 @@
-
+from quart_cors import cors
 from server.app import create_app
 
 
 if __name__ == "__main__":
-    app = create_app()
+    app = cors(create_app())
 
     app.run("0.0.0.0", 7777, debug=True)
