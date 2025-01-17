ea6cb9f55b52350ff78a76054de8edb987bc99d4
pyralasis <pyralas29@gmail.com>
2025-01-17 12:52:46 -0500

    init

---
+++ src/frontend/src/lib/components/General/FlyoutMenu.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    import { text } from "@sveltejs/kit";
+    import {
+        ButtonSizeEnum,
+        ButtonTypeEnum,
+        Flyout,
+        Header,
+        HeaderTypeEnum,
+        Popover,
+        PopoverChipTrigger,
+        PopoverMultiSelectContent,
+        TabContent,
+        TabLabel,
+        Tabs,
+        Button,
+        Accordion,
+        AccordionHeader,
+        AccordionBody,
+        Checkbox,
+        Text,
+        type PopoverItem,
+        Tag,
+        TagColorEnum,
+        TextInput,
+        PopoverSingleSelectContent,
+        InputSeries,
+    } from "kiwi-nl";
+
+    export let title;
+    export let description;
+</script>
