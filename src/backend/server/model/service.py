aa38faaaedf21a3c4cdf365284de2a543c7f7cf7
Gareth Fultz <garethfultz@gmail.com>
2025-01-23 17:33:42 -0500

    Updated model service constructor

--- src/backend/server/model/service.py
+++ src/backend/server/model/service.py
@@ -1,4 +1,6 @@
+from server.architecture.service import ArchitectureService
 from server.layer import LayerDefinition
+from server.layer.service import LayerService
 from server.util.registry import Registry
 
 
@@ -7,7 +9,9 @@ class ModelService:
     A service for managing PyTorch models.
     """
 
-    def __init__(self) -> None: ...
+    def __init__(self, layer_service: LayerService, architecture_service: ArchitectureService) -> None:
+        self.layer_service = layer_service
+        self.architecture_service = architecture_service
 
     def create_model(self) -> None:
         # Stub
