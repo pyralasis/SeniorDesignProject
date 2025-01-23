b4fa27c8a1b57de137c6985dc3f0177f41f225eb
Gareth Fultz <garethfultz@gmail.com>
2025-01-23 17:29:30 -0500

    Added model service stub

---
+++ src/backend/server/model/service.py
@@ -0,0 +1,14 @@
+from server.layer import LayerDefinition
+from server.util.registry import Registry
+
+
+class ModelService:
+    """
+    A service for managing PyTorch models.
+    """
+
+    def __init__(self) -> None: ...
+
+    def create_model(self) -> None:
+        # Stub
+        ...
