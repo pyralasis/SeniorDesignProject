9e2f3af8a0ac1d00ff09ba1630851727409df904
Calvin Kinateder <calvinkinateder@gmail.com>
2025-01-27 00:21:38 +0000

    Add MNIST

--- src/backend/server/data/sources/__init__.py
+++ src/backend/server/data/sources/__init__.py
@@ -1,6 +1,7 @@
 __all__ = ["default_sources", "DataSource", "DataSourceDefinition", "DataSourceDescription", "DataSourceId", "DS_ROOT"]
 
-from .mnist import fashion_mnist_values, fashion_mnist_labels
+from .fashion_mnist import fashion_mnist_values, fashion_mnist_labels
+from .mnist import mnist_values, mnist_labels
 from .base import DataSource, DataSourceDefinition, DataSourceDescription, DataSourceId, DS_ROOT
 
-default_sources: list[DataSourceDefinition] = [fashion_mnist_values, fashion_mnist_labels]
+default_sources: list[DataSourceDefinition] = [fashion_mnist_values, fashion_mnist_labels, mnist_values, mnist_labels]
---
+++ src/backend/server/data/sources/fashion_mnist.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+from typing import final
+from torchvision.datasets import FashionMNIST
+
+from server.data.sources.pytorch_base import TorchDatasetSource
+from server.data.sources.base import DS_ROOT, DataSourceDefinition
+from server.params import BoolParameter
+
+
+@final
+class FashionMNISTSource(TorchDatasetSource[FashionMNIST]):
+    def setup(self) -> None:
+        self.dataset.download()
+
+fashion_mnist_labels = DataSourceDefinition(
+    "fashion_mnist_labels",
+    "Fashion MNIST Labels",
+    (BoolParameter("train", "Use Training Data", True),),
+    lambda train, **_: FashionMNISTSource(FashionMNIST(Path(DS_ROOT, "fashion_mnist"), train), True),
+)
+
+fashion_mnist_values = DataSourceDefinition(
+    "fashion_mnist_values",
+    "Fashion MNIST Values",
+    (BoolParameter("train", "Use Training Data", True),),
+    lambda train, **_: FashionMNISTSource(FashionMNIST(Path(DS_ROOT, "fashion_mnist"), train), False),
+)
