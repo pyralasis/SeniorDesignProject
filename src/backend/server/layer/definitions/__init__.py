bd87f9b849c5f145690e9b0b3733fb722622744e
Gareth Fultz <garethfultz@gmail.com>
2025-02-27 16:53:57 -0500

    FIxed error

--- src/backend/server/layer/definitions/__init__.py
+++ src/backend/server/layer/definitions/__init__.py
@@ -1,17 +1,11 @@
 __all__ = ["default_layers"]
 
-from . import activation
-from . import convolution
-from . import linear
-from . import pooling
+from . import activation, convolution, linear, pooling
 
 default_layers = [
     activation.relu_layer,
-    
     convolution.conv2d_layer,
-
     linear.linear_layer,
-    linear.bilinear_layer,
-
-    pooling.max_pool2d_layer
+    # linear.bilinear_layer,
+    pooling.max_pool2d_layer,
 ]
