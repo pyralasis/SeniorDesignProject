a3c2a0d28f6eba94a832ee03a81fe46160bc1e90
pyralasis <pyralas29@gmail.com>
2025-03-06 15:44:47 -0500

    fix layer oopsie

--- src/backend/server/layer/definitions/__init__.py
+++ src/backend/server/layer/definitions/__init__.py
@@ -3,8 +3,14 @@ __all__ = ["default_layers"]
 from . import activation, convolution, linear, pooling
 
 default_layers = [
+    activation.elu_layer,
     activation.relu_layer,
+    activation.sigmoid_layer,
+    activation.softmin_layer,
+    activation.softmax_layer,
     convolution.conv2d_layer,
+    convolution.fold_layer,
+    convolution.unfold_layer,
     linear.linear_layer,
     # linear.bilinear_layer,
     pooling.max_pool2d_layer,
--- src/backend/server/layer/definitions/activation.py
+++ src/backend/server/layer/definitions/activation.py
@@ -6,58 +6,58 @@ from server.layer.size import size_identity
 
 # Found here: https://pytorch.org/docs/stable/nn.html#non-linear-activations-weighted-sum-nonlinearity
 
-# elu_layer = LayerDefinition(
-#     "elu",
-#     "ELU",
-#     (InputDefinition(None, 1, None),),
-#     (
-#         FloatParameter("alpha", "Alpha", 1.0),
-#         BoolParameter("inplace", "Inplace", False)
-#     ),
-#     lambda in_sizes, alpha, inplace, **_: torch.nn.ELU(alpha, inplace),
-#     lambda in_sizes, **_: in_sizes[0],
-# )
+elu_layer = LayerDefinition(
+    "elu",
+    "ELU",
+    (InputDefinition(None, 1, None),),
+    (
+        FloatParameter(id="alpha", name="Alpha", default=1.0),
+        BoolParameter("inplace", "Inplace", False),
+    ),
+    lambda in_sizes, alpha, inplace, **_: torch.nn.ELU(alpha, inplace),
+    lambda in_sizes, **_: in_sizes[0],
+)
 
 relu_layer = LayerDefinition(
     "relu",
     "ReLU",
     (InputDefinition(None, 1, None),),
     (
-        BoolParameter("inplace", "Inplace", False)
+        BoolParameter("inplace", "Inplace", False),
     ),
-    lambda in_sizes, inplace, **_: torch.nn.ReLU(),
+    lambda in_sizes, inplace, **_: torch.nn.ReLU(inplace),
     lambda in_sizes, **_: in_sizes[0],
 )
 
-# sigmoid_layer = LayerDefinition(
-#     "sigmoid",
-#     "Sigmoid",
-#     (InputDefinition(None, 1, None),),
-#     (),
-#     lambda in_sizes, **_: torch.nn.Sigmoid(),
-#     lambda in_sizes, **_: in_sizes[0],
-# )
+sigmoid_layer = LayerDefinition(
+    "sigmoid",
+    "Sigmoid",
+    (InputDefinition(None, 1, None),),
+    (),
+    lambda in_sizes, **_: torch.nn.Sigmoid(),
+    lambda in_sizes, **_: in_sizes[0],
+)
 
 # Found here: https://pytorch.org/docs/stable/nn.html#non-linear-activations-other
 
-# softmin_layer = LayerDefinition(
-#     "softmin",
-#     "Softmin",
-#     (InputDefinition(None, 1, None),),
-#     (
-#         IntParameter("dim", "Dim", 1)
-#     ),
-#     lambda in_sizes, dim, **_: torch.nn.Softmin(dim),
-#     lambda in_sizes, **_: in_sizes[0],
-# )
+softmin_layer = LayerDefinition(
+    "softmin",
+    "Softmin",
+    (InputDefinition(None, 1, None),),
+    (
+        IntParameter("dim", "Dim", 1)
+    ),
+    lambda in_sizes, dim, **_: torch.nn.Softmin(dim),
+    lambda in_sizes, **_: in_sizes[0],
+)
 
-# softmax_layer = LayerDefinition(
-#     "softmax",
-#     "Softmax",
-#     (InputDefinition(None, 1, None),),
-#     (
-#         IntParameter("dim", "Dim", 1)
-#     ),
-#     lambda in_sizes, dim, **_: torch.nn.Softmax(dim),
-#     lambda in_sizes, **_: in_sizes[0],
-# )
+softmax_layer = LayerDefinition(
+    "softmax",
+    "Softmax",
+    (InputDefinition(None, 1, None),),
+    (
+        IntParameter("dim", "Dim", 1)
+    ),
+    lambda in_sizes, dim, **_: torch.nn.Softmax(dim),
+    lambda in_sizes, **_: in_sizes[0],
+)
