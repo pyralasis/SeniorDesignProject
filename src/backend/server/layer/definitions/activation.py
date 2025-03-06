0142daef7b85cb5ddaccd296299339e39cfeb4ed
pyralasis <pyralas29@gmail.com>
2025-03-06 15:30:10 -0500

    oopsie occured

--- src/backend/server/layer/definitions/activation.py
+++ src/backend/server/layer/definitions/activation.py
@@ -1,22 +1,22 @@
 import torch
-from backend.server.params import BoolParameter, FloatParameter, IntParameter
+from server.params import BoolParameter, FloatParameter, IntParameter
 from server.layer import LayerDefinition
 from server.layer.input import InputDefinition
 from server.layer.size import size_identity
 
 # Found here: https://pytorch.org/docs/stable/nn.html#non-linear-activations-weighted-sum-nonlinearity
 
-elu_layer = LayerDefinition(
-    "elu",
-    "ELU",
-    (InputDefinition(None, 1, None),),
-    (
-        FloatParameter("alpha", "Alpha", 1.0),
-        BoolParameter("inplace", "Inplace", False)
-    ),
-    lambda in_sizes, alpha, inplace, **_: torch.nn.ELU(alpha, inplace),
-    lambda in_sizes, **_: in_sizes[0],
-)
+# elu_layer = LayerDefinition(
+#     "elu",
+#     "ELU",
+#     (InputDefinition(None, 1, None),),
+#     (
+#         FloatParameter("alpha", "Alpha", 1.0),
+#         BoolParameter("inplace", "Inplace", False)
+#     ),
+#     lambda in_sizes, alpha, inplace, **_: torch.nn.ELU(alpha, inplace),
+#     lambda in_sizes, **_: in_sizes[0],
+# )
 
 relu_layer = LayerDefinition(
     "relu",
@@ -29,35 +29,35 @@ relu_layer = LayerDefinition(
     lambda in_sizes, **_: in_sizes[0],
 )
 
-sigmoid_layer = LayerDefinition(
-    "sigmoid",
-    "Sigmoid",
-    (InputDefinition(None, 1, None),),
-    (),
-    lambda in_sizes, **_: torch.nn.Sigmoid(),
-    lambda in_sizes, **_: in_sizes[0],
-)
+# sigmoid_layer = LayerDefinition(
+#     "sigmoid",
+#     "Sigmoid",
+#     (InputDefinition(None, 1, None),),
+#     (),
+#     lambda in_sizes, **_: torch.nn.Sigmoid(),
+#     lambda in_sizes, **_: in_sizes[0],
+# )
 
 # Found here: https://pytorch.org/docs/stable/nn.html#non-linear-activations-other
 
-softmin_layer = LayerDefinition(
-    "softmin",
-    "Softmin",
-    (InputDefinition(None, 1, None),),
-    (
-        IntParameter("dim", "Dim", 1)
-    ),
-    lambda in_sizes, dim, **_: torch.nn.Softmin(dim),
-    lambda in_sizes, **_: in_sizes[0],
-)
+# softmin_layer = LayerDefinition(
+#     "softmin",
+#     "Softmin",
+#     (InputDefinition(None, 1, None),),
+#     (
+#         IntParameter("dim", "Dim", 1)
+#     ),
+#     lambda in_sizes, dim, **_: torch.nn.Softmin(dim),
+#     lambda in_sizes, **_: in_sizes[0],
+# )
 
-softmax_layer = LayerDefinition(
-    "softmax",
-    "Softmax",
-    (InputDefinition(None, 1, None),),
-    (
-        IntParameter("dim", "Dim", 1)
-    ),
-    lambda in_sizes, dim, **_: torch.nn.Softmax(dim),
-    lambda in_sizes, **_: in_sizes[0],
-)
+# softmax_layer = LayerDefinition(
+#     "softmax",
+#     "Softmax",
+#     (InputDefinition(None, 1, None),),
+#     (
+#         IntParameter("dim", "Dim", 1)
+#     ),
+#     lambda in_sizes, dim, **_: torch.nn.Softmax(dim),
+#     lambda in_sizes, **_: in_sizes[0],
+# )
