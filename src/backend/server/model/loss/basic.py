27be3e8c67aeff3847d96c2b5596c880496b17b9
Gareth Fultz <garethfultz@gmail.com>
2025-03-29 18:25:18 -0400

    Added basic loss function definitions

---
+++ src/backend/server/model/loss/basic.py
@@ -0,0 +1,27 @@
+import torch
+from server.model.loss.definition import LossDefinition
+from server.params import FloatParameter
+
+# NLL Loss
+nll_loss = LossDefinition(
+    "nll_loss",
+    "Negative Log Likelihood Loss",
+    (),
+    lambda **params: torch.nn.NLLLoss(**params),
+)
+
+# Cross Entropy
+cross_entropy_loss = LossDefinition(
+    "cross_entropy",
+    "Cross Entropy Loss",
+    (),
+    lambda **params: torch.nn.CrossEntropyLoss(**params),
+)
+
+# Mean Squared Error
+mse_loss = LossDefinition(
+    "mse_loss",
+    "Mean Squared Error Loss",
+    (),
+    lambda **params: torch.nn.MSELoss(**params),
+)
