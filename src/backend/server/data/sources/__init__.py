ab6047e52851058180d8bbe0984bd4a7c14b40ff
pyralasis <pyralas29@gmail.com>
2025-04-07 14:28:02 -0400

    fix small bug in EMNIST definition

--- src/backend/server/data/sources/__init__.py
+++ src/backend/server/data/sources/__init__.py
@@ -4,5 +4,6 @@ from .fashion_mnist import fashion_mnist_values, fashion_mnist_labels
 from .mnist import mnist_values, mnist_labels
 from .base import DataSource, DataSourceDefinition, DataSourceDescription, DataSourceId, DS_ROOT
 from .csv import csv_source
+from .emnist import emnist_labels, emnist_values
 
-default_sources: list[DataSourceDefinition] = [fashion_mnist_values, fashion_mnist_labels, mnist_values, mnist_labels, csv_source]
+default_sources: list[DataSourceDefinition] = [fashion_mnist_values, fashion_mnist_labels, mnist_values, mnist_labels, csv_source, emnist_values, emnist_labels]
--- src/backend/server/data/sources/emnist.py
+++ src/backend/server/data/sources/emnist.py
@@ -3,7 +3,7 @@ from typing import final
 
 from server.data.sources.base import DS_ROOT, DataSourceDefinition
 from server.data.sources.pytorch_base import TorchDatasetSource
-from server.params import BoolParameter
+from server.params import BoolParameter, StringParameter
 from torchvision.datasets import EMNIST
 
 
@@ -14,13 +14,13 @@ class EMNISTSource(TorchDatasetSource[EMNIST]): ...
 emnist_labels = DataSourceDefinition(
     "emnist_labels",
     "EMNIST Labels",
-    (BoolParameter("train", "Use Training Data", True),),
-    lambda train, **_: EMNISTSource(EMNIST(Path(DS_ROOT, "emnist"), train, download=True), True),
+    (StringParameter("split", "Split", ""), BoolParameter("train", "Use Training Data", True),),
+    lambda split, train, **_: EMNISTSource(EMNIST(Path(DS_ROOT, "emnist"), split, train=train, download=True), True),
 )
 
 emnist_values = DataSourceDefinition(
     "emnist_values",
     "EMNIST Values",
-    (BoolParameter("train", "Use Training Data", True),),
-    lambda train, **_: EMNISTSource(EMNIST(Path(DS_ROOT, "emnist"), train, download=True), False),
+    (StringParameter("split", "Split", ""), BoolParameter("train", "Use Training Data", True),),
+    lambda split, train, **_: EMNISTSource(EMNIST(Path(DS_ROOT, "emnist"), split, train=train, download=True), False),
 )
